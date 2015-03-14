6ad69b44e03d31b3c82740267108a323ae8ffc7c
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-14 15:47:44 +0800

    Add shared brocfile import for font-awesome

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ module.exports = {
   name: 'shared.goodcity',
   isDevelopingAddon: function() {
     return true;
+  },
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import("bower_components/font-awesome/css/font-awesome.css");
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
+    app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
   }
 };
