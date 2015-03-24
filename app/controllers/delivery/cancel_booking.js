2cadfcbd6479c293d45537244572be4acfccde72
swatijadhav <swati.jadhav@kiprosh.com>
2015-03-24 13:31:25 +0530

    links on cancel-booking page

--- app/controllers/delivery/cancel_booking.js
+++ app/controllers/delivery/cancel_booking.js
@@ -1,13 +1,23 @@
 import Ember from 'ember';
+import config from './../../config/environment';
+
 export default Ember.ObjectController.extend({
+
+  needs: ['offer/transport_details'],
 
   canCancel: Ember.computed.alias('model.gogovanOrder.isCancelled'),
 
   driverContact: Ember.computed.alias('model.gogovanOrder.driverMobile'),
 
+  isAdmin: function(){
+    return config.APP.NAME === 'admin.goodcity';
+  }.property(),
+
   actions: {
     cancelBooking: function() {
-
+      if(this.get('canCancel')){
+        this.get('controllers.offer/transport_details').send('removeDelivery', this.get('model'));
+      }
     }
   }
 });
