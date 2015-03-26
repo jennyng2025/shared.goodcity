9627fcafa263c26bee62a89864d7d7c5c3aab130
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-03-26 11:38:51 +0800

    Supply missing arguments to js expression, 'this' should still refer to first argument if it's value is false

--- app/helpers/js-x.js
+++ app/helpers/js-x.js
@@ -1,5 +1,5 @@
 export default Ember.HTMLBars.makeBoundHelper(function (params) {
   var paramNames = params.slice(1).map(function(val, idx) { return "p" + idx; });
   var func = Function.apply(this, paramNames.concat("return " + params[0] + ";"))
-  return func.apply(params[1] || this);
+  return func.apply(params[1] === undefined ? this : params[1], params.slice(1));
 });
