39ada38a889b1b0c2a470d26af9e99d66cb98e45
sunil sharma <sunil.sharma@kiprosh.com>
2015-03-27 19:03:53 +0530

    GCW-212 comment#fixed index route and offer status

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,4 @@
 import SessionRoute from './session';
 
 export default SessionRoute.extend({
-  beforeModel: function(messages) {
-    if(this.get("session.isAdmin")) {
-      this.transitionTo('login');
-    }
-  }
 });
