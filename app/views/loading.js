3090f3ccba381863735f47c7658de210ddd66224
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-04-22 16:39:24 +0800

    GCW-293 Add logging of loading view timeout to Errbit

--- app/views/loading.js
+++ app/views/loading.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export default Ember.View.extend({
   templateName: 'loading',
   alert: Ember.inject.service(),
+  logger: Ember.inject.service(),
   timer: null,
 
   onInit: function() {
     this.set("timer", Ember.run.later(() => {
+      this.get("logger").error(new Error("Loading view timeout reached."));
       this.get("alert").show(Ember.I18n.t("loading_timeout"), () => {
         this.destroy();
         window.location.reload();
