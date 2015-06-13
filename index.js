8951d4463a50a2459ed01483e18838317d91ca7f
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-06-13 12:39:17 +0800

    Add https://dc.services.visualstudio.com to content policy used by testfairy

--- index.js
+++ index.js
@@ -20,18 +20,27 @@ module.exports = {
   },
 
   contentFor: function(type, config) {
-    // add content security policy meta tag wanted by cordova-plugin-whitelist
-    // add winstore-jscompat.js for windows phone (not needed for Windows 10) https://github.com/MSOpenTech/winstore-jscompat
     if (type === 'head-footer' && config.cordova.enabled) {
-      // gap: required by ios
-      config.contentSecurityPolicy["frame-src"] = (config.contentSecurityPolicy["frame-src"] || "") + " gap:";
-      // unsafe-self required by cordova.js
-      config.contentSecurityPolicy["script-src"] = (config.contentSecurityPolicy["script-src"] || "") + " 'unsafe-eval'";
-      var policy = Object.keys(config.contentSecurityPolicy)
-        .map(function(key) { return key + " " + config.contentSecurityPolicy[key]; })
-        .join("; ");
-      return '<meta http-equiv="Content-Security-Policy" content="' + policy + '">' +
+      // add content security policy meta tag wanted by cordova-plugin-whitelist
+      // add winstore-jscompat.js for windows phone (not needed for Windows 10) https://github.com/MSOpenTech/winstore-jscompat
+      return '<meta http-equiv="Content-Security-Policy" content="' + this.getContentPolicy(config) + '">' +
         '\n<script src="shared.goodcity/winstore-jscompat.js"></script>';
     }
+  },
+
+  getContentPolicy: function(config) {
+    var cordovaPolicy = {
+      "frame-src": "gap:", // required by ios
+      "script-src": "'unsafe-eval'", // required by cordova.js
+      "connect-src": "https://dc.services.visualstudio.com" // required by testfairy
+    };
+
+    Object.keys(cordovaPolicy).forEach(function(key) {
+      config.contentSecurityPolicy[key] = (config.contentSecurityPolicy[key] || "") + " " + cordovaPolicy[key];
+    });
+
+    return Object.keys(config.contentSecurityPolicy)
+      .map(function(key) { return key + " " + config.contentSecurityPolicy[key]; })
+      .join("; ");
   }
 };
