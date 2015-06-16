a2b26d95a2e1289ff03ac7e34c4c0b697803833e
swatijadhav <swati.jadhav@kiprosh.com>
2015-06-16 19:41:25 +0530

    style changes to include audio-links in messages

--- app/helpers/apply-line-break.js
+++ app/helpers/apply-line-break.js
@@ -1,7 +1,12 @@
 import Ember from "ember";
 
 export default Ember.Handlebars.makeBoundHelper(function(value) {
-  var text = Handlebars.Utils.escapeExpression(value);
-  text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+  var text;
+  if(/<[a-z][\s\S]*>/i.test(value)) {
+    text = value;
+  } else {
+    var text = Handlebars.Utils.escapeExpression(value);
+    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+  }
   return new Handlebars.SafeString(text);
 });
