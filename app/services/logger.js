275a9b6808c3059a2331709fb54a1e006d4317e0
Michael McDonald <none@none>
2015-07-23 15:59:08 +0800

    GCW-500 upgrade airbrake-js client

--- app/services/logger.js
+++ app/services/logger.js
@@ -9,13 +9,18 @@ export default Ember.Service.extend({
       return;
     }
     console.info(reason);
-    if (config.environment === "production") {
+    if (config.environment === "production" || config.staging) {
       var userName = this.get("session.currentUser.fullName");
       var userId = this.get("session.currentUser.id");
       var error = reason instanceof Error || typeof reason != "object" ?
           reason : JSON.stringify(reason);
 
-      Airbrake.push({
+      var airbrake = new airbrakeJs.Client();
+      airbrake.setHost(config.APP.AIRBRAKE_HOST);
+      airbrake.setProject(config.APP.AIRBRAKE_PROJECT_ID, config.APP.AIRBRAKE_PROJECT_KEY);
+      airbrake.setEnvironmentName(config.staging ? "staging" : config.environment);
+
+      airbrake.push({
         error: error,
         context: { userId: userId, userName: userName }
       });
