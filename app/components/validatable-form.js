1056bb84116b8eb97860b19a94357c3bf554d8a4
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-08-19 16:07:47 +0800

    GCW-568 fix error sometimes thrown when logging in

--- app/components/validatable-form.js
+++ app/components/validatable-form.js
@@ -3,7 +3,9 @@ import ValidatableForm from 'ember-cli-html5-validation/components/validatable-f
 // Can be removed once https://github.com/maestrooo/ember-cli-html5-validation/issues/21 is fixed
 export default ValidatableForm.extend({
   submit(event) {
-    event.preventDefault();
+    if (event) { // fix for https://jira.crossroads.org.hk/browse/GCW-568 not something I can reproduce
+      event.preventDefault();
+    }
     return this._super();
   }
 });
