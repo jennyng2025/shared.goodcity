fa75f1d8baf24bbbd19a493d806ff7ca2b57852c
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-21 13:11:22 +0530

    GCW-582 comment# fixed issue with language-switcher

--- app/components/language-switcher.js
+++ app/components/language-switcher.js
@@ -13,6 +13,7 @@ export default Ember.Component.extend({
   actions: {
     setLanguage: function(language) {
       this.set('session.language', language);
+      window.location.reload();
     }
   }
 
