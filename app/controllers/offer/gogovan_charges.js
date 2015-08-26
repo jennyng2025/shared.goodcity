649c91b156f14226fd99b7e88de373141bbe5549
swatijadhav <swati.jadhav@kiprosh.com>
2015-08-26 11:32:30 +0530

    GCW-590 comment# fixed ggv booking charges info page

--- app/controllers/offer/gogovan_charges.js
+++ app/controllers/offer/gogovan_charges.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   i18n: Ember.inject.service(),
   info: function() {
-    return "<div>" + this.get("i18n").t("gogovan_charges.info").replace(/\n\n/g, "</div><div>") + "</div>";
+    var chargesInfo = this.get("i18n").t("gogovan_charges.info").string;
+    return "<div>" + chargesInfo.replace(/\n\n/g, "</div><div>") + "</div>";
   }.property()
 });
