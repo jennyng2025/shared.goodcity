19ad3eb28602cd6b2bb7eefd4aadfc4b95e6becf
swatijadhav <swati.jadhav@kiprosh.com>
2015-09-13 19:51:22 +0530

    removed init.js view

--- app/components/init-foundation.js
+++ app/components/init-foundation.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   didInsertElement() {
+
+    Ember.run.debounce(this, function(){
+      var clientHeight = $( window ).height();
+      $('.inner-wrap').css('min-height', clientHeight);
+    }, 1000);
+
     Ember.$(document).foundation({
       offcanvas: { close_on_click: true }
     });
