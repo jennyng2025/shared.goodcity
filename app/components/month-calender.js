0bc4f1c40a44a1c83606fdabc6c73675e5ef7618
swatijadhav <swati.jadhav@kiprosh.com>
2015-09-14 15:52:00 +0530

    GCW-557 comment# drop-off delivery: allow next available slot selection for current-day

--- app/components/month-calender.js
+++ app/components/month-calender.js
@@ -1,9 +1,25 @@
 import Ember from 'ember';
 
+// Handle time selection based on current time
+// <select id="ember1325" required="">
+//   <option value="">Time</option>
+//   <option value="1">9AM-11AM</option> => (540mins - 660mins)
+//   <option value="2">11AM-1PM</option> => (660mins - 840mins)
+//   <option value="3">2PM-4PM</option>  => (840mins)
+// </select>
+
 export default Ember.TextField.extend({
   tagName: 'input',
   classNames: 'pickadate',
   attributeBindings: [ "name", "type", "value", "id", 'required', 'pattern', 'available', 'placeholder' ],
+
+  currentMinutes: function(){
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes() > 30 ? 30 : 0;
+    var total_mins = hours*60 + minutes;
+    return (total_mins > 840) ? 840 : total_mins;
+  },
 
   didInsertElement: function(){
     var _this = this;
@@ -21,6 +37,9 @@ export default Ember.TextField.extend({
         date_array.push(date.getDate());
         available_array.push(date_array);
       }
+
+      var currentMins = _this.currentMinutes();
+      if(currentMins === 840) { available_array.pop(); }
     }
 
     Ember.$().ready(function(){
@@ -30,7 +49,6 @@ export default Ember.TextField.extend({
         monthsShort: moment.monthsShort(),
         weekdaysShort: moment.weekdaysShort(),
         disable: available_array,
-        min: new Date(),
         clear: false,
         today: false,
         close: false,
@@ -48,6 +66,44 @@ export default Ember.TextField.extend({
             this.set('select', new Date(date), { format: 'ddd mmm d' });
             setting = false;
           });
+
+          if(date) {
+            var selectedDate = date;
+            var currentDate = new Date();
+            currentDate.setHours(0,0,0,0);
+            selectedDate.setHours(0,0,0,0);
+
+            if(selectedDate.getTime() === currentDate.getTime()) {
+              var total_mins = _this.currentMinutes();
+              var option;
+
+              if(total_mins >= 540 && total_mins < 660) {
+                option = Ember.$(".time_selector select option:eq(1)");
+              } else if(total_mins >= 660 && total_mins < 840) {
+                option =  Ember.$(".time_selector select option:eq(2)");
+              } else if(total_mins >= 840) {
+                option = Ember.$(".time_selector select option:eq(3)");
+              }
+
+              if(option) {
+                option.addClass("hidden");
+                option[0].disabled = true;
+
+                option.prevAll().each(function() {
+                  Ember.$( this ).addClass("hidden");
+                  this.disabled = true;
+                });
+              }
+
+            } else {
+              // Enable all select options
+              Ember.$(".time_selector select option").each(function() {
+                Ember.$( this ).removeClass("hidden");
+                this.disabled = false;
+              });
+            }
+          }
+
         },
 
         onStart: function(){
