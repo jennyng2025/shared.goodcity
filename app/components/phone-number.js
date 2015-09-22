7cde08f7edc0897e0935e433f96a3fef20cd30a8
swatijadhav <swati.jadhav@kiprosh.com>
2015-09-22 20:10:42 +0530

    moved phone view to component

--- app/components/phone-number.js
+++ app/components/phone-number.js
@@ -1,6 +1,6 @@
 import Ember from "ember";
 
-export default Ember.View.extend({
+export default Ember.Component.extend({
   tagName: "span",
   number: null,
   hidden: Ember.computed.empty("number"),
