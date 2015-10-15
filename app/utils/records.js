06e8b928cd9ada5aed51f46fbf79423b31cca50e
swatijadhav <swati.jadhav@kiprosh.com>
2015-10-15 15:14:54 +0530

    fixed issue with deletion of rejected items

--- app/utils/records.js
+++ app/utils/records.js
@@ -5,10 +5,13 @@ export default {
   // this function navigates the relationship tree unloading records unless
   // related record is a taxonomy type or user
   unloadRecordTree: function(record) {
-    var taxonomyTypes = config.APP.PRELOAD_TYPES
-      .concat(config.APP.PRELOAD_AUTHORIZED_TYPES)
-      .concat("user")
-      .map(t => t.replace(/_/, '-'));
+    var taxonomyTypes = ["territory", "package-type","donor-condition","rejection-reason","permission", "timeslot", "gogovan-transport", "crossroads-transport", "version", "user"]
+
+    // TO_DO FIX ISSUE WITH DELETION
+    // var taxonomyTypes = config.APP.PRELOAD_TYPES
+    //   .concat(config.APP.PRELOAD_AUTHORIZED_TYPES)
+    //   .concat("user")
+    //   .map(t => t.replace(/_/, '-'));
 
     if (!record || record.unloading || taxonomyTypes.indexOf(record.constructor.modelName) !== -1) {
       return;
