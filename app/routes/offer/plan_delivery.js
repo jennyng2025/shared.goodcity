ba5978cbfe0d158a1152793ad11a7017e7fd7c23
swatijadhav <swati.jadhav@kiprosh.com>
2015-10-19 17:38:19 +0530

    fixed plan-transport link if offer is not available

--- app/routes/offer/plan_delivery.js
+++ app/routes/offer/plan_delivery.js
@@ -10,12 +10,16 @@ export default AuthorizeRoute.extend({
     var offerId = this.modelFor('offer').get('id');
     var offer = this.store.peekRecord('offer', offerId);
 
-    if (offer.get('isScheduled') && !params.queryParams.modify) {
-      if(this.get('session.isAdminApp')) {
-        this.transitionTo('review_offer.logistics', offer);
-      } else {
-        this.transitionTo('offer.transport_details', offer);
+    if(offer) {
+      if (offer.get('isScheduled') && !params.queryParams.modify) {
+        if(this.get('session.isAdminApp')) {
+          this.transitionTo('review_offer.logistics', offer);
+        } else {
+          this.transitionTo('offer.transport_details', offer);
+        }
       }
+    } else {
+      this.transitionTo("offers");
     }
   }
 });
