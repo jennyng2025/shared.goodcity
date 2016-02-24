6510efc341ed666cde8a583b8f8a65210962c5fc
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-02-24 12:55:14 +0800

    GCW-864 Improve list rendering performance by reducing number of setInterval functions with timeago

--- app/components/time-ago.js
+++ app/components/time-ago.js
@@ -1,42 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
-  didInsertElement() {
-
-    if(this.smallPrefix) {
-      Ember.$.timeago.settings.strings.suffixAgo = "";
-      Ember.$.timeago.settings.strings.suffixFromNow = "";
-      Ember.$.timeago.settings.strings.seconds = this.locale("1m");
-      Ember.$.timeago.settings.strings.minute = this.locale("1m");
-      Ember.$.timeago.settings.strings.minutes = "%d" + this.locale("m");
-      Ember.$.timeago.settings.strings.hour = this.locale("1h");
-      Ember.$.timeago.settings.strings.hours = "%d" + this.locale("h");
-      Ember.$.timeago.settings.strings.day = this.locale("1d");
-      Ember.$.timeago.settings.strings.days = "%d" + this.locale("d");
-      Ember.$.timeago.settings.strings.month = this.locale("1mths");
-      Ember.$.timeago.settings.strings.months = "%d" + this.locale("mths");
-      Ember.$.timeago.settings.strings.year = this.locale("1y");
-      Ember.$.timeago.settings.strings.years = "%d" + this.locale("y");
-    }
-
-    Ember.$().ready(function (){
-      Ember.$('time.timeago').timeago();
-      // update every minute
-      setInterval((function() {
-        return Ember.$("time.timeago").timeago();
-      }), 60000);
-    });
-  },
-
-  i18n: Ember.inject.service(),
-
-  locale: function(str) {
-    return this.get("i18n").t("time_ago." + str).string;
-  },
-
-  timeString: Ember.computed(function(){
+  time: Ember.computed(function() {
     var timeValue = this.attrs.timeValue.value || new Date();
-    return new Date(timeValue).toISOString();
+    return timeValue.toISOString();
+  }),
+
+  timeDisplay: Ember.computed(function() {
+    var timeValue = this.attrs.timeValue.value || new Date();
+    return moment(timeValue).fromNow(true);
   })
 });
---
+++ app/instance-initializers/timeago.js
@@ -0,0 +1,39 @@
+import Ember from "ember";
+import config from '../config/environment';
+
+export default {
+  name: 'timeago',
+
+  initialize: function(app) {
+    var i18n = app.container.lookup("service:i18n");
+    var loc = str => i18n.t("time_ago." + str).string;
+
+    moment.locale('en', {
+      relativeTime : {
+        future: "",
+        past:   "",
+        s:  loc("1m"),
+        m:  loc("1m"),
+        mm: "%d" + loc("m"),
+        h:  "%d" + loc("1h"),
+        hh: "%d" + loc("h"),
+        d:  "%d" + loc("1d"),
+        dd: "%d" + loc("d"),
+        M:  "%d" + loc("1mths"),
+        MM: "%d" + loc("mths"),
+        y:  "%d" + loc("1y"),
+        yy: "%d" + loc("y")
+      }
+    });
+
+    if (config.environment != "test") {
+      Ember.run.later(this, this.updateTime, 60000);
+    }
+  },
+
+  updateTime: function() {
+    Ember.$(".timeago").each((idx, elm) =>
+      elm.innerText = moment(elm.getAttribute("datetime")).fromNow(true));
+    Ember.run.later(this, this.updateTime, 60000);
+  }
+};
