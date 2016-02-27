b962aa815cb791e4f2392d3364d6fb7bd5d898b9
Michael McDonald <mcm-ham@users.noreply.github.com>
2016-02-27 17:40:06 +0800

    Add ability to define inner template for message box

--- app/components/message-box.js
+++ app/components/message-box.js
@@ -3,22 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   layoutName: 'components/message-box',
+  message: "",
   btn1Text: "",
   btn1Callback: () => {},
   btn2Text: "",
   btn2Callback: () => {},
+  messageComponent: null,
+  messageModal: null,
 
   actions: {
     btn1Click: function() {
       if (this.btn1Callback) {
-        this.btn1Callback();
+        this.btn1Callback(messageModal);
       }
       this.destroy();
     },
 
     btn2Click: function() {
       if (this.btn2Callback) {
-        this.btn2Callback();
+        this.btn2Callback(messageModal);
       }
       this.destroy();
     }
